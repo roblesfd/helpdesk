d84890f84d77a2d50eba7de912700b5a3061df11
roblesfd <fernandrobhdz@gmail.com>
2024-07-18 12:02:04 -0600

    Se modifica var env para la URL del backend en apiSlice

--- src/app/api/apiSlice.js
+++ src/app/api/apiSlice.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { setCredentials } from "../../features/auth/authSlice";
 
 const baseQuery = fetchBaseQuery({
-  baseUrl: "https://helpdesk-frontend-onat.onrender.com/",
+  baseUrl: process.env.VITE_BACKEND_URL_PRO,
   credentials: "include",
   prepareHeaders: (headers, { getState }) => {
     const token = getState().auth.token;
